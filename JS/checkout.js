e2e242e8891fb167e2091986944df92e36bf2f10
Brogy-alt <brogangys2@gmail.com>
2022-12-06 15:34:51 +0200

    updated fixed admin , worked on product page

---
+++ JS/checkout.js
@@ -0,0 +1,22 @@
+let submit = document.querySelector("button");
+
+// // Add event listener
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    
+    let submit = document.querySelector('#submit');
+    let display = document.querySelector('#display');
+
+
+//     // Push an object info an array
+    products.push(
+        {
+            id,
+            product,
+            beattype
+        }
+    );
+    console.log(products);
+
+})
---
+++ JS/code.js
@@ -0,0 +1,49 @@
+// Sample data: example
+let products = JSON.parse(localStorage.getItem('products ')) ?
+JSON.parse(localStorage.getItem('products ')) : [
+    
+    {
+        id:1,
+        product:"Beat",
+        beattype:"Juice Wrld",
+        price:30
+
+    },
+    {
+        id:2,
+        product:"Beat",
+        beattype:"Lil Uzi Vert",
+        price:30
+
+    },
+    {
+        id:3,
+        product:"Beat",
+        beattype:"Iann Doir",
+        price:30
+
+    },
+];
+
+
+function displayProducts(){
+let tbody = document.querySelector('tbody');
+    Object.keys(products).forEach( (item)=>{
+        if(products.length){
+            console.log(`${item}: ${products[item]}`);
+            tbody.innerHTML +=
+            `
+                <tr>
+                    <td>${products[item].id}</td>
+                    <td>${products[item].product}</td>
+                    <td>${products[item].beattype}</td>
+                    <td>${products[item].price}</td>
+                </tr>
+            `
+        }
+    } )
+
+}
+
+displayProducts()
+
